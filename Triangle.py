cbfbf24120803e69ff7ee803365b89096ca36bca
Nathalie Souza <69611410+nathsouza@users.noreply.github.com>
2022-10-12 10:56:03 -0400

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -4,33 +4,26 @@
 #Homework Assignment #2
 
 
-def classifyTriangle(a,b,c):
+def classify_triangle(a,b,c):
     """
-    Your correct code goes here...  Fix the faulty logic below until the code 
-passes all of 
-    you test cases. 
-    
-    This function returns a string with the type of triangle from three integer 
-values
+    Your correct code goes here...  Fix the faulty logic below until the code passes all of you test cases.
+    This function returns a string with the type of triangle from three integer values
     corresponding to the lengths of the three sides of the Triangle.
-    
     return:
         If all three sides are equal, return 'Equilateral'
         If exactly one pair of sides are equal, return 'Isoceles'
         If no pair of  sides are equal, return 'Scalene'
         If not a valid triangle, then return 'NotATriangle'
-        If the sum of any two sides equals the squate of the third side, then 
-        return 'Right'
+        If the sum of any two sides equals the squate of the third side, then return 'Right'
 
     BEWARE: there may be a bug or two in this code
     """
     # require that the input values be >= 0 and <= 200
     # if a <= 200 or b <= 200 or c <= 200:
     #     return 'InvalidInput'
-        
+
     if a <= 0 or b <= 0 or c <= 0:
         return 'InvalidInput'
-    
     # verify that all 3 inputs are integers  
     # Python's "isinstance(object,type) returns True if the object is of the specified type
     if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
